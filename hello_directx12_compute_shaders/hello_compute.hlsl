56fe09376e719b5b5590271efb887f942287531a
Liam Wynn <wynnliam@pdx.edu>
2024-11-22 16:17:40 -0800

    Fixed name for SV_DispatchThreadID arg

--- hello_directx12_compute_shaders/hello_compute.hlsl
+++ hello_directx12_compute_shaders/hello_compute.hlsl
@@ -1,14 +1,14 @@
 RWTexture2D<float4> buffer : register(u0);
 
 [numthreads(8, 8, 1)]
-void main( uint3 DTid : SV_DispatchThreadID )
+void main( uint3 dispatch_thread_id : SV_DispatchThreadID )
 {
     uint width;
     uint height;
     float2 uv;
     
     buffer.GetDimensions(width, height);
-    uv = dispatchThreadID.xy / float2(width, height);
+    uv = dispatch_thread_id.xy / float2(width, height);
     
-    buffer[dispatchThreadID.xy] = float4(uv.xy, 0.0f, 1.0f);
+    buffer[dispatch_thread_id.xy] = float4(uv.xy, 0.0f, 1.0f);
 }
